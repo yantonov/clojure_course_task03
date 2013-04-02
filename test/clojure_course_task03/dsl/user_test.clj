fc54b9897a0b7699bdf18274765ff094e0f2e19e
Yury Antonov <yantonov@yandex.ru>
2013-04-02 08:30:23 +0600

    Unnesessary macroexpand calls were removed from tests.

--- test/clojure_course_task03/dsl/user_test.clj
+++ test/clojure_course_task03/dsl/user_test.clj
@@ -12,15 +12,13 @@
 (test/deftest user-macro-defines-variables-contained-table-privileges
   (test/testing "User macro defines variables contained table privileges."
     (let [ns-sym (symbol (ns-name *ns*))]
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.group/group
-               Agent
-               proposal -> [person, phone, address, price]
-               agents -> [client_id, proposal_id, agent])))
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.user/user
-               Ivanov
-               (belongs-to Agent))))
+      (eval '(clojure-course-task03.dsl.group/group
+              Agent
+              proposal -> [person, phone, address, price]
+              agents -> [client_id, proposal_id, agent]))
+      (eval '(clojure-course-task03.dsl.user/user
+              Ivanov
+              (belongs-to Agent)))
       (test/is (false? (nil? (ns-resolve ns-sym
                                          'Ivanov-proposal-fields-var))))
       (test/is (false? (nil? (ns-resolve ns-sym
@@ -30,15 +28,13 @@
 (test/deftest variables-defined-by-user-macro-contains-table-privileges
   (test/testing "Variables defined by user macro contains table privileges."
     (let [ns-sym (symbol (ns-name *ns*))]
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.group/group
-               Agent
-               proposal -> [person, phone, address, price]
-               agents -> [client_id, proposal_id, agent])))
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.user/user
-               Ivanov
-               (belongs-to Agent))))
+      (eval '(clojure-course-task03.dsl.group/group
+              Agent
+              proposal -> [person, phone, address, price]
+              agents -> [client_id, proposal_id, agent]))
+      (eval '(clojure-course-task03.dsl.user/user
+              Ivanov
+              (belongs-to Agent)))
       (test/is (= [:person, :phone, :address, :price]
                   @(ns-resolve ns-sym 'Ivanov-proposal-fields-var)))
       (test/is (= [:client_id, :proposal_id, :agent]
@@ -46,15 +42,13 @@
 
 
 (test/deftest user-macro-updates-user-tables-var-collection
-  (eval (macroexpand
-         '(clojure-course-task03.dsl.group/group
-           Agent
-           proposal -> [person, phone, address, price]
-           agents -> [client_id, proposal_id, agent])))
-  (eval (macroexpand
-         '(clojure-course-task03.dsl.user/user
-           Ivanov
-           (belongs-to Agent))))
+  (eval '(clojure-course-task03.dsl.group/group
+          Agent
+          proposal -> [person, phone, address, price]
+          agents -> [client_id, proposal_id, agent]))
+  (eval '(clojure-course-task03.dsl.user/user
+          Ivanov
+          (belongs-to Agent)))
   (let [vars (target/get-user-tables-vars)]
     (test/is (contains? vars 'Ivanov-agents-fields-var))
     (test/is (contains? vars 'Ivanov-proposal-fields-var))
@@ -64,26 +58,21 @@
 (test/deftest attach-to-multiple-groups
   (test/testing "user macro can be used to attach user to multiple groups"
     (let [ns-sym (symbol (ns-name *ns*))]
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.group/group
-               A
-               a_table -> [a_column])))
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.group/group
-               B
-               b_table -> [b_column])))
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.group/group
-               C
-               c_table -> [c_column])))
-      (eval (macroexpand
-             '(clojure-course-task03.dsl.user/user
-               Ivanov
-               (belongs-to A B C))))
+      (eval '(clojure-course-task03.dsl.group/group
+              A
+              a_table -> [a_column]))
+      (eval '(clojure-course-task03.dsl.group/group
+              B
+              b_table -> [b_column]))
+      (eval '(clojure-course-task03.dsl.group/group
+              C
+              c_table -> [c_column]))
+      (eval '(clojure-course-task03.dsl.user/user
+              Ivanov
+              (belongs-to A B C)))
       (test/is (false? (nil? (ns-resolve ns-sym
                                          'Ivanov-a_table-fields-var))))
       (test/is (false? (nil? (ns-resolve ns-sym
                                          'Ivanov-b_table-fields-var))))
       (test/is (false? (nil? (ns-resolve ns-sym
-                                         'Ivanov-c_table-fields-var))))
-      )))
+                                         'Ivanov-c_table-fields-var)))))))
